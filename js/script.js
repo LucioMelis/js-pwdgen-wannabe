7010362fdc2c485644ed4c33f496c29248e9a0f2
luciomelis <luciomelis1992@gmail.com>
2022-03-03 16:22:06 +0100

    Finish

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+// Control JS 
 console.log('Js OK');
+
+// Section Name 
+const firstName = prompt('Inserisci il tuo nome');
+console.log('firstName');
+
+// Section Surname 
+const surName = prompt('Inserisci il tuo cognome');
+console.log('SurName');
+
+// Section Color 
+const colorPrefer = prompt('Inserisci il tuo colore preferito');
+console.log('colorPrefer');
+
+// Stringa su Console 
+console.log('Questa è la tua password:' + firstName + surName + colorPrefer + '21');
+
+// Stringa sul codice HTML 
+document.getElementById('password').innerText = `Questa è la tua password:${firstName}${surName}${colorPrefer}21`;
